4690b15515aa02403736ab03af0415609081489d
Simon Wears <simon.wears@degree53.com>
2017-09-03 16:12:12 +0100

    feat(*): Update test application to include proxy mode
    Updated the test application to run in a proxy mode if no RunKeeper keys are found

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const session = require('express-session');
 const healthgraphql = require('./src/healthgraphql');
 const passport = require('passport');
 const RunKeeperStrategy = require('passport-runkeeper').Strategy;
+const cors = require('cors');
 
 const RUNKEEPER_CLIENT_ID = process.env.RUNKEEPER_CLIENT_ID;
 const RUNKEEPER_CLIENT_SECRET = process.env.RUNKEEPER_CLIENT_SECRET;
@@ -11,36 +12,51 @@ const PORT = process.env.PORT || 3000;
 const SECURE = process.env.SECURE === 'true';
 const HOSTNAME = process.env.HOSTNAME;
 
+const PROXY = !RUNKEEPER_CLIENT_ID || !RUNKEEPER_CLIENT_SECRET || !COOKIE_SECRET;
+
 const app = express();
 
-passport.serializeUser((user, done) => done(null, user));
-passport.deserializeUser((obj, done) => done(null, obj));
-passport.use(new RunKeeperStrategy({
-        clientID: RUNKEEPER_CLIENT_ID,
-        clientSecret: RUNKEEPER_CLIENT_SECRET,
-        callbackURL: `${SECURE ? 'https' : 'http' }://${HOSTNAME}:${PORT}/auth/callback`
-    },
-    (accessToken, refreshToken, profile, done) => done(null, {
-        access_token: accessToken,
-        id: profile.id
-    })
-));
+if (!PROXY) {
+    passport.serializeUser((user, done) => done(null, user));
+    passport.deserializeUser((obj, done) => done(null, obj));
+    passport.use(new RunKeeperStrategy({
+            clientID: RUNKEEPER_CLIENT_ID,
+            clientSecret: RUNKEEPER_CLIENT_SECRET,
+            callbackURL: `${SECURE ? 'https' : 'http' }://${HOSTNAME}:${PORT}/auth/callback`
+        },
+        (accessToken, refreshToken, profile, done) => done(null, {
+            access_token: accessToken,
+            id: profile.id
+        })
+    ));
 
-app.use(session({
-    secret: COOKIE_SECRET,
-    resave: false,
-    saveUninitialized: true,
-    cookie: {secure: SECURE}
-}));
-app.use(passport.initialize());
-app.use(passport.session());
+    app.use(session({
+        secret: COOKIE_SECRET,
+        resave: false,
+        saveUninitialized: true,
+        cookie: {secure: SECURE}
+    }));
+    app.use(passport.initialize());
+    app.use(passport.session());
 
-app.use('/graphql', healthgraphql({ getAccessToken: req => req.user ? req.user.access_token : null }));
-app.get('/auth', passport.authenticate('runkeeper'), () => {});
-app.get('/auth/callback', passport.authenticate('runkeeper', {failureRedirect: '/auth/runkeeper'}),
-    (req, res) => { res.redirect('/graphql'); });
-app.get('/logout', (req, res) => { req.logout(); res.redirect('/'); });
+    app.get('/auth', passport.authenticate('runkeeper'), () => {});
+    app.get('/auth/callback', passport.authenticate('runkeeper', {failureRedirect: '/auth/runkeeper'}), (req, res) => { res.redirect('/graphql'); });
+    app.get('/logout', (req, res) => {
+        req.logout();
+        res.redirect('/');
+    });
+} else {
+    app.use(cors());
+}
 
-app.listen(PORT, () => { console.log(
-`Application started - ${SECURE ? 'https' : 'http' }://${HOSTNAME}:${PORT}/graphql
-Login at ${SECURE ? 'https' : 'http' }://${HOSTNAME}:${PORT}/auth`); });
+app.use('/graphql', healthgraphql(
+    PROXY ? {} : {getAccessToken: req => req.user ? req.user.access_token : null})
+);
+
+app.listen(PORT, () => {
+    console.log(`Application started - ${SECURE ? 'https' : 'http' }://${HOSTNAME}:${PORT}/graphql`);
+
+    if (!PROXY) {
+        console.log(`Login at ${SECURE ? 'https' : 'http' }://${HOSTNAME}:${PORT}/auth`);
+    }
+});
